4634760f04f81e9dd70572f2c41495893cd790ff
John-Zoscak-02 <jzoscak02@gmail.com>
2024-03-11 18:11:51 -0400

    default tiling layout

--- config.h
+++ config.h
@@ -55,10 +55,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "@",        spiral },    /* first entry is default */
+    { "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-    { "[]=",      tile },
+    { "@",        spiral },    
     { "[\\]",     dwindle },
 };
 
